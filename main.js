477942bf82d61bdbe6f46b40ad69a12ea1e5e930
gerogarcia03 <gerogarcia2003@gmail.com>
2022-06-02 20:51:51 -0300

    segundo commit

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ if (precio==24){
 } else if (precio<24){
     alert("El precio " + precio + " es menor a 24");
 }
+
+
+let numero=prompt("numero al azar");
+
+if(numero>1000){
+    alert("Numero es mayor a 1000");
+} else if(numero<1000){
+    alert("Numero menor a 1000");
+}
