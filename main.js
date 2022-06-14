6ff4bf0ac89a69104d37d9f298c20e92dd9092ae
gerogarcia03 <gerogarcia2003@gmail.com>
2022-06-14 17:43:34 -0300

    tercer commit

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+//Pedir un número y sumarle 
+let ingresaNumero = parseInt(prompt("Por favor ingrese un número: "));
+
+for (let i=1; i<=20; i++) {
+    let resultado = ingresaNumero + i;
+    alert(resultado);
+}
+
+
+
+
+
 
