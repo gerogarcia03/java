a658e72234bf631fb74b907702cc08beb9acaf99
gerogarcia03 <gerogarcia2003@gmail.com>
2022-06-21 20:47:44 -0300

    cuarto commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let productoName = ``;
 let cantidadTotal = 0;
 let cantidad = 0;
 let precio = 0;
-let nombre = "";
+let nombre = ``;
 let precioTotal= 0;
 let seguirComprando = false;
 
@@ -23,6 +23,7 @@ function compra(){
         producto = prompt(`Hola ${nombre}
         ¿Qué desea comprar?
         Por favor, ingrese el número del producto.
+
         1- Coca 500 $100
         2- Pepitos $150
         3- Coca Cola Sin Azucar 2.25 $330
@@ -43,66 +44,51 @@ function compra(){
                 precio = 330;
                 productoName = `Coca Cola Sin Azucar 2.25`
                 break;
-            case `3`:
+            case `4`:
                 precio = 200;
                 productoName = `Pepsi Black 1.5`
                 break;
-            case `4`:
+            case `5`:
                 precio = 90;
                 productoName = `Duquesa`
                 break;
             default:
                 nombre=null;
-                alert(`Su nombre no ha sido registrado, por favor, vuelva a ingresar`)
-                welcome();
+                cantidad=0;
+                productoName= ``;
                 precio=0;
-                producto=0;
                 alert(`Algunos de los datos ingresados es erroneo. Por favor, 
                 vuelva a realizar la compra`)
-                compra();
+                welcome();
         }
-        Cantidad();
 }
 
-function Cantidad(){
-    
-        cantidad = Number(prompt(nombre + `¿Cuántos ` + productoName + `s desea llevar? 
+function Cantidad() {
+     
+        cantidad = Number(prompt(`${nombre} ¿Cuántos ` + productoName + `s desea llevar? 
         Ingrese en numeros, cuantos `+ productoName+ `s desea llevar`));
-    
 
-        // let canastaProductos =+ ` | ` + producto;
-        // let canstaCantidades =+ ` | ` + cantidad;
-        let precioTotal =+ precio*cantidad;
-
-        if (cantidad=1){
-            finalizarCompra();
-        }
 }
 
-// function continuarCompra(){
-
-//     alert(`Acaba de comprar ` + cantidad + ` ` + productoName);
-
-//     let seguirComprando = !confirm (`¿Desea seguir comprando?`)
-
-//     if ( seguirComprando ){
-//         finalizarCompra();                                                                             
-//     }
-// }
+Cantidad();
 
 function finalizarCompra() {
 
-    alert(`Su compra ha finalizado con exito! Usted ha comprado` + `\n\n` + mensaje + 
-    `\n\n`+ `y el total es $ ` + precioTotal);
+    let precioTotal =+ precio*cantidad;
 
-    let finalizar = confirm (`¿Desea finalizar la compra?`);
+    alert(`Su compra ha finalizado con exito! Usted ha comprado ` + cantidad + ` ` + productoName + ` y el total es $` + precioTotal);
 
-    if ( finalizar ){
+
+    if ( seguirComprando ){
 
     alert(`¡Su compra ha finalizado con exito! 
     ¡Gracias por comprar en Kioskiri!`);
 
     }
+}
+
+if (cantidad>=1){
+    finalizarCompra();
 }
 
 
